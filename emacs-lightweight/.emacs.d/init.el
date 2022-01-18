e66ff8640a856a1768e5dc156d8ff3c4bcc5ae4f
No000 <sk.777.main.9@gmail.com>
2022-01-18 13:18:53 +0900

    カーソル移動の軽量化

--- emacs-lightweight/.emacs.d/init.el
+++ emacs-lightweight/.emacs.d/init.el
@@ -82,7 +82,23 @@
   ;; (setq ring-bell-function 'ignore)
 
   ;; barを出るようにする
-  (global-hl-line-mode t) 
+  ;; (global-hl-line-mode t)
+  (require 'hl-line)
+  
+;;; hl-lineを無効にするメジャーモードを指定する
+  ;; (defvar global-hl-line-timer-exclude-modes '(todotxt-mode))
+  (defvar global-hl-line-timer-exclude-modes nil)
+  (defun global-hl-line-timer-function ()
+    (unless (memq major-mode global-hl-line-timer-exclude-modes)
+      (global-hl-line-unhighlight-all)
+      (let ((global-hl-line-mode t))
+        (global-hl-line-highlight))))
+  (setq global-hl-line-timer
+        (run-with-idle-timer 0.03 t 'global-hl-line-timer-function))
+  ;; (cancel-timer global-hl-line-timer)
+
+
+  
 
   ;; 現状スクロールバーとメニューバーを使っていないため削除する。
   ;; 可能であればyaskrollのようなものに変更を行いたい。
