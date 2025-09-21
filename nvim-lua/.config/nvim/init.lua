e75fbc3993384840d310d733f63db03471c1eff4
toto <toto.1010cat@outlook.jp>
2025-09-22 00:05:06 +0900

    jk or klでnormal mode

--- nvim-lua/.config/nvim/init.lua
+++ nvim-lua/.config/nvim/init.lua
@@ -4,3 +4,5 @@ vim.scriptencding = "utf-8"
 vim.wo.number = true
 vim.o.clipboard = vim.o.clipboard .. 'unnamedplus'
 vim.o.autoindent = true
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true })
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true })
